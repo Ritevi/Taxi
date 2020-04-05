371eab947357dba5758d08cb8e80d83513bfe998
Ritevi <sanekv1999@gmail.com>
2020-04-06 02:33:25 +0300

    start Serializer

--- libs/Serializer.js
+++ libs/Serializer.js
@@ -1,39 +1,48 @@
-var Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
+module.exports = class Serializer {
+  defaultUnsafeParams = ["password"];
+  constructor(dataToSerialize, unSafeParams = this.defaultUnsafeParams) {
+    this._dataToSerialize = dataToSerialize;
+    this._unSafeParams = unSafeParams;
+    this._serializedData = {};
+  }
 
-var Serializer = async function (instance,options = {}) {
-    if(!instance) 
-        throw new Error("NO_INSTANCE");
+  async Serialize(options) {
     try {
-        const allAttr = Object.keys(instance.get({plain:true}));
-        var attributes = options.attributes||[];
-        var include = options.include||[];
-        var exclude = options.exclude||[];
-        var result ={};
+      const allAttrs = Object.keys(this._dataToSerialize);
+    } catch (err) {}
+  }
+};
 
-        if(attributes){
-            exclude = [];
-            for(let attr in attributes){
-                result[attr]=instance.get(attr);
-            }
-        }
-
-        if(exclude){
-            let othersAttr = allAttr;
-            othersAttr.filter((attr)=>{
-                return exclude.includes(attr);
-            });
-            for(let attr in othersAttr){
-                result[attr]=instance.get(attr);
-            }
-        }
-
-        if(include){
-
-
-        }
-
-    } catch (e) {
-        
-    }
-}
+//
+// const Serializer = async function (instance, options = {}) {
+//   if (!instance) throw new Error("NO_INSTANCE");
+//   try {
+//     const allAttr = Object.keys(instance.get({ plain: true }));
+//     var attributes = options.attributes || [];
+//     var include = options.include || [];
+//     var exclude = options.exclude || [];
+//     var result = {};
+//
+//     if (attributes) {
+//       exclude = [];
+//       for (let attr in attributes) {
+//         result[attr] = instance.get(attr);
+//       }
+//     }
+//
+//     if (exclude) {
+//       let othersAttr = allAttr;
+//       othersAttr.filter((attr) => {
+//         return exclude.includes(attr);
+//       });
+//       for (let attr in othersAttr) {
+//         result[attr] = instance.get(attr);
+//       }
+//     }
+//
+//     if (include) {
+//     }
+//   } catch (e) {}
+// };
